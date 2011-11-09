61e099d508847d75ca4e2b536204b906fa36a0de
Martin Omander <momander@google.com>
2011-11-09 14:51:01 -0800

    Made the unit tests a little more realistic by testing a class (Adder)

---
+++ scuttlebutt/adder.py
@@ -0,0 +1,6 @@
+
+class Adder():
+
+  def add(self, number1, number2):
+    return number1 + number2
+
--- scuttlebutt/tests.py
+++ scuttlebutt/tests.py
@@ -1,15 +1,31 @@
 import sys
 import unittest
 
-class Test_Addition(unittest.TestCase):
+class Test_Adder(unittest.TestCase):
 
-  def setUp(self):
-    pass
-  
-  def test_addition(self):
-    self.assertEqual(4, 2+2)
+  def test_add(self):
+    """ Tests that the Adder class adds correctly. """
+    from adder import Adder
+    a = Adder()
+    expected_result = 5
+    actual_result = a.add(2, 3)
+    self.assertEqual(expected_result, actual_result)
 
 
+
+
+""" When run without parameters like this:
+      python tests.py
+    all unit tests in this file are run.
+
+    When run with one parameter like this:
+      python tests.py Test_Adder
+    all unit tests in the Test_Adder case are run.
+
+    When run with two parameters like this:
+      python tests.py Test_Adder test_add
+    only the method test_add within Test_Adder is run.
+"""
 if __name__ == '__main__':
   if len(sys.argv) == 2:
     test = eval("%s" % (sys.argv[1]))
