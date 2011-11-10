b2a7f97c579f9af8ac99d0c798c6fe8650f9ab7f
Martin Omander <momander@google.com>
2011-11-10 10:40:54 -0800

    Made HelloWorld output be UTF-8, so non-ASCII characters show up correctly

--- scuttlebutt/helloworld.py
+++ scuttlebutt/helloworld.py
@@ -7,7 +7,7 @@ from google.appengine.ext.webapp import util
 
 class HelloWorldHandler(webapp.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.headers['Content-Type'] = 'text/plain;charset=utf-8'
     self.response.out.write(u'こんにちは、世界！')
 
 
